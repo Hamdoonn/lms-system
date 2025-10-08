68d45085b850c35e3cf66511da853b9c15310d2b
hus9075 <9075hu@gmail.com>
2025-10-09 00:59:12 +0500

    done

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -23,6 +23,7 @@ import Reports from "@/pages/admin/Reports";
 import Users from "@/pages/admin/Users";
 // InstructorDashboard
 import InstructorDashboard from "@/pages/instructore/InstructorDashboard";
+// import Courses from "@/pages/instructore/Courses";
 import AddCourses from "@/pages/instructore/courses/AddCourses";
 import ManageCourses from "@/pages/instructore/courses/ManageCourses";
 import Students from "@/pages/instructore/Students";
@@ -83,7 +84,7 @@ const Router = () => {
           <Route index element={<AdminDashboard />} />
           <Route path="/admin/reports" element={<Reports />} />
           <Route path="/admin/users" element={<Users />} />
-          <Route path="/admin/courses" element={<Courses />}/>
+          {/* <Route path="/admin/courses" element={<Courses />}/> */}
         </Route>
       </Routes>
     </BrowserRouter>
