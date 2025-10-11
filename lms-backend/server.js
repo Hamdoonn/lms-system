d2ded109e1b783f8f54e2cfe9cc5c90c87c02131
Hamdoonn <hamdoonshahid3@gmail.com>
2025-10-11 05:14:28 -0700

    added role base middleware + auth

--- lms-backend/server.js
+++ lms-backend/server.js
@@ -2,10 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 
+import userRoutes from "./src/modules/user/user-routes.js";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+//routes
+app.use("/api/users", userRoutes);
 
 //connect mongodb
 connectDB();
---
+++ lms-backend/src/middleware/auth-middleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  try {
+    let token;
+
+    //check auth header
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    if (!token) {
+      return res.status(401).json({ message: "No token, unauthorized" });
+    }
+
+    //verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+export const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "Access denied, insufficient permission" });
+    }
+    next();
+  };
+};
