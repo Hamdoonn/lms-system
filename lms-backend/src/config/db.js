1948c32af25140da203442a1828d6319e0f68418
Safwan <safwanshahid15122004@gmail.com>
2025-10-18 16:14:00 +0500

    temp

--- lms-backend/src/config/db.js
+++ lms-backend/src/config/db.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI,{
+      dbName: "lms-cluster"
+    });
+    console.log("MongoDB connected to lms-cluster DB");
   } catch (error) {
     console.error("MonogDB connection error", error.message);
     process.exit(1);
